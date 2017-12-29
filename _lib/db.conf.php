3b86af650a429f9c40a6b38f437502e7b14c2cd0
Unknown <unknown@example.com>
2017-12-29 03:28:03 +0200

    adjustment

--- _lib/db.conf.php
+++ _lib/db.conf.php
@@ -10,7 +10,7 @@ $username = 'root';
 $password = '';
 $db_name = 'lammah_db';
 
-$conn = @mysqli_connect($server_name, $username, $password, $db_name);
+$conn = mysqli_connect($server_name, $username, $password, $db_name);
 mysqli_set_charset($conn, 'utf8');
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
