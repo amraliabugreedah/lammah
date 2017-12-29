3ba50dcc52bae2bcb6a88d4514110060b851229a
Unknown <unknown@example.com>
2017-12-29 04:09:53 +0200

    changed table of orders style

--- project/get_user_orders.php
+++ project/get_user_orders.php
@@ -31,17 +31,14 @@ while($row = $result->fetch_assoc()){
     $result2 = $conn->query($sql2);
 
     echo "<div class=\"row table-responsive\" align=\"center\" style=\"display: block;\" id=''>";
+    echo "<div class='col-sm-6'>";
     echo "<table class=\"table table-hover table-bordered table-striped jquery-tablesorter\">";
     echo " <thead>
       <tr>
         <th class=\"text-center\" style='width: 2%'></th>
-        <th class=\"text-center\" style='width: 8%'>Order ID</th>
         <th class=\"text-center clickableElem\" data-sort=\"text\" style='width: 13%'>Item Name <i class=\"fa fa-sort\" aria-hidden=\"true\"></i></th>
         <th class=\"text-center clickableElem\" data-sort=\"num\" style='width: 9%'>Item Price <i class=\"fa fa-sort\" aria-hidden=\"true\"></i></th>
         <th class=\"text-center clickableElem\" data-sort=\"num\" style='width: 11%'>Item Quantity <i class=\"fa fa-sort\" aria-hidden=\"true\"></i></th>
-        <th class=\"text-center\" style='width: 20%'>Order Status</th>
-        <th class=\"text-center\" style='width: 15%'>Order Creation Time</th>
-        <th class=\"text-center\" style='width: 15%'>Expected Delivery Date</th>
       </tr>
     </thead>";
     echo "<tbody>";
@@ -52,34 +49,54 @@ while($row = $result->fetch_assoc()){
         $total_quantity = $total_quantity + $row2["quantity"];
         echo "<tr id=''>";
         echo "  <td align='center' id=''></td>
-                            <td align='center' id=''>" . $row2['id'] . "</td>
                             <td align='center' id=''>" . $row2["item_name"] . "</td>
                             <td align='center' id=''>" . $row2["item_price"] . "</td>
                             <td align='center' id=''>" . $row2["quantity"] . "</td>";
-        if ($row2["status"] == 1) {
-            echo "<td align='center' id=''>Delivered</td>";
-        } else if ($row2["status"] == 0) {
-            echo "<td align='center' id=''>Order Not Delivered Yet</td>";
-        } else if ($row2["status"] == -1) {
-            echo "<td align='center' id=''>Cancelled</td>";
-        }
-        echo "   <td align='center' id=''>" . $row2["creation_time"] . "</td>
-        
-        <td align='center' id=''>" . $row2["expected_delivery_time"] . "</td>";
         echo "</tr>";
+    $order_id = $row2['id'];
+    $order_status = $row2["status"];
+    $order_creation_time = $row2["creation_time"];
+    $order_expected_time = $row2["expected_delivery_time"];
     }
     echo "<tr class='staticRow'>";
     echo " <td align='center' id=''>Total</td>
                             <td align='center' id=''></td>
-                           <td align='center' id=''></td>
                             <td align='center' id=''>$total_price</td>
                             <td align='center' id=''>$total_quantity</td>
-                            <td align='center' id=''></td>
-                            <td align='center' id=''></td>
                                     </tr>";
     echo "</tbody>";
-    echo " </table>  <hr class=\"style2\">";
-    echo "</div>";
+    echo " </table></div>";
+    echo "<div class='col-sm-6'>";
+    echo "<table class=\"table table-hover table-bordered table-striped jquery-tablesorter\">";
+    echo " <thead>
+      <tr>
+        <th class=\"text-center\" >Title</th>
+        <th class=\"text-center\" data-sort=\"text\" >Information </th>
+      </tr>
+    </thead>";
+    echo "<tbody>";
+    echo "<tr id=''>";
+    echo "     <td align='center' id=''>Order ID</td>  <td align='center' id=''>" . $order_id. "</td></tr>";
+    echo "<tr id=''>";
+
+    echo "     <td align='center' id=''>Order Status</td>";
+    if ($order_status == 1) {
+        echo "<td align='center' id=''>Delivered</td>";
+    } else if ($order_status == 0) {
+        echo "<td align='center' id=''>Order Not Delivered Yet</td>";
+    } else if ($order_status == -1) {
+        echo "<td align='center' id=''>Cancelled</td>";
+    }
+    echo"</tr>";
+
+    echo "<tr id=''>";
+    echo "     <td align='center' id=''>Order Creation Time</td>   <td align='center' id=''>" .$order_creation_time . "</td></tr>";
+    echo "<tr id=''>";
+    echo "     <td align='center' id=''>Expected Delivery Date</td> <td align='center' id=''>" .$order_expected_time . "</td></tr>";
+
+    echo "</tbody>";
+    echo " </table> </div> ";
+    echo "</div> <hr class=\"style2\"> ";
 }
 
 ?>
