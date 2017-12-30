26374b0b79eecf86e3ad00e6cf4e329086bcac6b
Unknown <unknown@example.com>
2017-12-31 01:14:56 +0200

    hide order in nav and show it on request

--- _inc/nav.php
+++ _inc/nav.php
@@ -11,9 +11,9 @@
 echo "<ul>
   <li><a id=\"main\" href=\"../project/main.php\">Main Page</a></li>
   <li><a id=\"newUser\" href=\"../project/new_user.php\">New User</a></li>
-  <li><a id=\"order\" href=\"../project/order.php\">Order</a></li>
   <li><a id=\"users\" href=\"../project/users.php\">Users</a></li>
   <li><a id=\"food\" href=\"../project/products.php\">Products</a></li>
+  <li><a id=\"order\" style='display: none' href=\"../project/order.php\">Order</a></li>
   <li class='pull-right'><a id=\"logout\" href=\"../_auth/logout.php\">logout</a></li>
 </ul>";
 
